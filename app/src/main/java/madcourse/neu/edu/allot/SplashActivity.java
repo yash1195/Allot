d89fb763a2d75be73162a7cac59e901f15090e2b
Nay Htet <htet.na@husky.neu.edu>
2017-11-23 23:52:45 -0800

    Animation added

--- app/src/main/java/madcourse/neu/edu/allot/SplashActivity.java
+++ app/src/main/java/madcourse/neu/edu/allot/SplashActivity.java
@@ -3,14 +3,21 @@ package madcourse.neu.edu.allot;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class SplashActivity extends AppCompatActivity {
     private static final int SPLASH_SCREEN_TIME = 4000;
+    private ImageView logo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        logo = (ImageView) findViewById(R.id.imageView);
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.splash);
+        logo.startAnimation(animation);
         Thread mainThread = new Thread() {
             @Override
             public void run() {
